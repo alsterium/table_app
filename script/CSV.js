9883b4ca7c3be40c9d32c59fd083bd174723f3f0
alsterium <new033@gmail.com>
2020-01-07 14:54:25 +0900

    add files

---
+++ script/CSV.js
@@ -0,0 +1,52 @@
+// function getCSV() {
+//     var dataArray;
+//     var req = new XMLHttpRequest();
+//     req.open("get", "http://www.mn.cis.iwate-u.ac.jp/~nakaya/report/Data.csv", true);
+//     req.send(null);
+
+//     req.onload = function() {
+//         dataArray = convertCSVtoArray(req.responseText);
+//     }
+//     console.log(dataArray);
+//     return dataArray;
+// }
+
+// function convertCSVtoArray(str) {
+//     var result = [];
+//     var tmp = str.split("\n");
+
+//     for (var i = 0; i < tmp.length; ++i) {
+//         result[i] = tmp[i].split(',');
+//     }
+//     return result;
+// }
+
+window.onload = () => {
+    new Vue({
+        el: "#tableApp",
+        data: {
+            tableData: []
+        },
+        methods: {
+            getCSV: function() {
+                var req = new XMLHttpRequest();
+                req.open(
+                    "get",
+                    "http://www.mn.cis.iwate-u.ac.jp/~nakaya/report/Data.csv",
+                    true
+                );
+                req.send(null);
+
+                req.onload = function() {
+                    this.convertCSVtoArray(req.responseText);
+                };
+            },
+            convertCSVtoArray: function(str) {
+                var tmp = str.split("\n");
+                for (var i = 0; i < tmp.length; ++i) {
+                    tableData[i] = tmp[i].split(",");
+                }
+            }
+        },
+    });
+}
